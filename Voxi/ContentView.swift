37c9fc16e7d3ce5085a2281b41eba5eae123f375
Rodolfo Gonzalez <rodolfogonzalez@Rodolfos-MacBook-Air.local>
2024-10-29 00:57:35 -0300

    Update

--- Voxi/ContentView.swift
+++ Voxi/ContentView.swift
@@ -9,127 +9,127 @@ import SwiftUI
 
 /*
  struct ContentView: View {
-     @EnvironmentObject private var speechViewModel : SpeechViewModel
-     @State private var showSaveDeleteOptions = false
-     
-     var body: some View {
-         NavigationView {
-             VStack {
-                 ZStack(alignment: .topLeading) {
-                     RoundedRectangle(cornerRadius: 10)
-                         .fill(Color(.lightGray))
-                         .frame(maxWidth: .infinity, maxHeight: .infinity)
-                     
-                     
-                      //codigo para implementar cuando esten las transcripciones
-                      if speechViewModel.currentTranscription.isEmpty && speechViewModel.isAnalyzing {
-                          Text("Escuchando")
-                              .foregroundColor(.gray)
-                              .padding(.horizontal, 16)
-                              .padding(.top, 12)
-                      }
-                             
-                      
-                      
-                    
-                     Text(speechViewModel.currentTranscription)
-                         .padding(.horizontal, 16)
-                         .padding(.top, 12)
-                 }
-                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                 .padding()
-                 
-                 if showSaveDeleteOptions {
-                     HStack(spacing: 16) {
-                         Button(action: {
-                             speechViewModel.saveTranscription()
-                             withAnimation {
-                                 showSaveDeleteOptions = false
-                             }
-                         }) {
-                             HStack {
-                                 Image(systemName: "checkmark.circle.fill")
-                                     .imageScale(.medium)
-                                 Text("Guardar")
-                             }
-                             .frame(maxWidth: .infinity)
-                             .padding(.vertical, 8)
-                             .background(Color.green)
-                             .foregroundColor(.white)
-                             .cornerRadius(8)
-                         }
-                         Button(action: {
-                             DispatchQueue.main.async {
-                                 speechViewModel.currentTranscription = ""
-                             }
-                             
-                             withAnimation {
-                                 showSaveDeleteOptions = false
-                             }
-                         }) {
-                             HStack {
-                                 Image(systemName: "trash.fill")
-                                     .imageScale(.medium)
-                                 Text("Cancelar")
-                             }
-                             .frame(maxWidth: .infinity)
-                             .padding(.vertical, 8)
-                             .background(Color.red)
-                             .foregroundColor(.white)
-                             .cornerRadius(8)
-                         }
-                     }
-                     .padding(.horizontal)
-                     .padding(.top)
-                 } else {
-                     Button(action: toggleAnalysis) {
-                         HStack {
-                             Image(systemName: speechViewModel.isAnalyzing ? "stop.fill" : "mic.fill")
-                                 .imageScale(.medium)
-                             Text(speechViewModel.isAnalyzing ? "Detener análisis" : "Iniciar análisis")
-                         }
-                         .frame(maxWidth: .infinity)
-                         .padding(.vertical, 8)
-                         .background(speechViewModel.isAnalyzing ? Color.red : Color.green)
-                         .foregroundColor(.white)
-                         .cornerRadius(8)
-                     }
-                     .padding(.horizontal)
-                     .padding(.top)
-                 }
-                 NavigationLink(destination: CategoriesView()) {
-                     Text("Ver categorias")
-                         .font(.headline)
-                         .foregroundColor(.primary)
-                         .padding()
-                     
-                 }
-                 .onReceive(speechViewModel.$isAuthorized) { isAuthorized in
-                                     if isAuthorized {
-                                         DispatchQueue.main.async {
-                                             toggleAnalysis()
-                                         }
-                                         
-                                     }
-                                 }
-             }
-             .navigationTitle("Transcripción")
-         }
-     }
-     private func toggleAnalysis() {
-         if speechViewModel.isAnalyzing {
-             showSaveDeleteOptions = !speechViewModel.currentTranscription.isEmpty
-             speechViewModel.requestStopAnalysis()
-             
-             
-         } else {
-             if !speechViewModel.isAuthorized {
-                 speechViewModel.requestStartAuthorization()
-             } else {
-                 speechViewModel.requestStartAnalysis()
-             }
-         }
-     }
+ @EnvironmentObject private var speechViewModel : SpeechViewModel
+ @State private var showSaveDeleteOptions = false
+ 
+ var body: some View {
+ NavigationView {
+ VStack {
+ ZStack(alignment: .topLeading) {
+ RoundedRectangle(cornerRadius: 10)
+ .fill(Color(.lightGray))
+ .frame(maxWidth: .infinity, maxHeight: .infinity)
+ 
+ 
+ //codigo para implementar cuando esten las transcripciones
+ if speechViewModel.currentTranscription.isEmpty && speechViewModel.isAnalyzing {
+ Text("Escuchando")
+ .foregroundColor(.gray)
+ .padding(.horizontal, 16)
+ .padding(.top, 12)
+ }
+ 
+ 
+ 
+ 
+ Text(speechViewModel.currentTranscription)
+ .padding(.horizontal, 16)
+ .padding(.top, 12)
+ }
+ .frame(maxWidth: .infinity, maxHeight: .infinity)
+ .padding()
+ 
+ if showSaveDeleteOptions {
+ HStack(spacing: 16) {
+ Button(action: {
+ speechViewModel.saveTranscription()
+ withAnimation {
+ showSaveDeleteOptions = false
+ }
+ }) {
+ HStack {
+ Image(systemName: "checkmark.circle.fill")
+ .imageScale(.medium)
+ Text("Guardar")
+ }
+ .frame(maxWidth: .infinity)
+ .padding(.vertical, 8)
+ .background(Color.green)
+ .foregroundColor(.white)
+ .cornerRadius(8)
+ }
+ Button(action: {
+ DispatchQueue.main.async {
+ speechViewModel.currentTranscription = ""
+ }
+ 
+ withAnimation {
+ showSaveDeleteOptions = false
+ }
+ }) {
+ HStack {
+ Image(systemName: "trash.fill")
+ .imageScale(.medium)
+ Text("Cancelar")
+ }
+ .frame(maxWidth: .infinity)
+ .padding(.vertical, 8)
+ .background(Color.red)
+ .foregroundColor(.white)
+ .cornerRadius(8)
+ }
+ }
+ .padding(.horizontal)
+ .padding(.top)
+ } else {
+ Button(action: toggleAnalysis) {
+ HStack {
+ Image(systemName: speechViewModel.isAnalyzing ? "stop.fill" : "mic.fill")
+ .imageScale(.medium)
+ Text(speechViewModel.isAnalyzing ? "Detener análisis" : "Iniciar análisis")
+ }
+ .frame(maxWidth: .infinity)
+ .padding(.vertical, 8)
+ .background(speechViewModel.isAnalyzing ? Color.red : Color.green)
+ .foregroundColor(.white)
+ .cornerRadius(8)
+ }
+ .padding(.horizontal)
+ .padding(.top)
+ }
+ NavigationLink(destination: CategoriesView()) {
+ Text("Ver categorias")
+ .font(.headline)
+ .foregroundColor(.primary)
+ .padding()
+ 
+ }
+ .onReceive(speechViewModel.$isAuthorized) { isAuthorized in
+ if isAuthorized {
+ DispatchQueue.main.async {
+ toggleAnalysis()
+ }
+ 
+ }
+ }
+ }
+ .navigationTitle("Transcripción")
+ }
+ }
+ private func toggleAnalysis() {
+ if speechViewModel.isAnalyzing {
+ showSaveDeleteOptions = !speechViewModel.currentTranscription.isEmpty
+ speechViewModel.requestStopAnalysis()
+ 
+ 
+ } else {
+ if !speechViewModel.isAuthorized {
+ speechViewModel.requestStartAuthorization()
+ } else {
+ speechViewModel.requestStartAnalysis()
+ }
+ }
+ }
  }
  */
 
@@ -210,8 +210,8 @@ struct ContentView: View {
                     
                     Button(action: {
                         withAnimation {
-                                toggleAnalysis()
-                            }
+                            toggleAnalysis()
+                        }
                     }) {
                         HStack {
                             Image(systemName: (viewModel.buttonValue == true) && (viewModel.isSpeechAuthorized == .authorized) && (viewModel.isMicrophoneAuthorized == true) ? "stop.fill" : "mic.fill")
@@ -227,13 +227,13 @@ struct ContentView: View {
                     .padding(.horizontal)
                     .padding(.top)
                     
-                }
-                
-                NavigationLink(destination: CategoriesView()) {
-                    Text("Ver categorias")
-                        .font(.headline)
-                        .foregroundColor(.primary)
-                        .padding()
+                    NavigationLink(destination: CategoriesView()) {
+                        Text("Ver categorias")
+                            .font(.headline)
+                            .foregroundColor(.primary)
+                            .padding()
+                            .opacity(!viewModel.buttonValue ? 1 : 0)
+                    }
                 }
             }
             
