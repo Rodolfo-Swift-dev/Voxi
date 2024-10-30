050aaf154f6a36333e4f5b2b0c6c9d927fe5fe92
Rodolfo Gonzalez <rodolfogonzalez@Rodolfos-MacBook-Air.local>
2024-10-30 16:50:00 -0300

    update MVVM

--- Voxi/SpeechRecognizer.swift
+++ Voxi/SpeechRecognizer.swift
@@ -10,159 +10,6 @@ import SwiftUI
 import Combine
 import Speech
 
-/*
- class SpeechRecognizer {
-     private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "es-ES"))
-     private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
-     private var recognitionTask: SFSpeechRecognitionTask?
-     private let audioEngine = AVAudioEngine()
-     
-     private let startAuthorizationSubject = PassthroughSubject<Void, Never>()
-     let authorizationStatusPublisher = PassthroughSubject<Bool, Never>()
-     
-     private let startRecognitionSubject = PassthroughSubject<Void, Never>()
-     let speechResultPublisher = PassthroughSubject<String, Never>()
-     let speechErrorPublisher = PassthroughSubject<Void, Never>()
-     
-     private let stoptRecognitionSubject = PassthroughSubject<Void, Never>()
-     
-     private var cancellables = Set<AnyCancellable>()
-     
-     init() {
-         startAuthorizationSubject.sink { [weak self] in
-             self?.startRequestAuthorization()
-         }
-         .store(in: &cancellables)
-         
-         startRecognitionSubject.sink { [weak self] in
-             self?.startRecognition()
-         }
-         .store(in: &cancellables)
-         
-         stoptRecognitionSubject.sink { [weak self] in
-             self?.stoptRecognition()
-         }
-         .store(in: &cancellables)
-     }
-     
-     private func startRequestAuthorization() {
-         SFSpeechRecognizer.requestAuthorization { status in
-             DispatchQueue.main.async {
-                 switch status {
-                 case .authorized:
-                     self.authorizationStatusPublisher.send(true)
-                 case .denied, .restricted, .notDetermined:
-                     self.authorizationStatusPublisher.send(false)
-                 @unknown default:
-                     fatalError("Unknown authorization status")
-                 }
-             }
-         }
-     }
-     private func startRecognition() {
-         // Cancelamos cualquier tarea de reconocimiento en progreso
-         recognitionTask?.cancel()
-         recognitionTask = nil
-         
-         //clase responsable de gestionar la configuración y el comportamiento de la sesión de audio de tu aplicación
-         let audioSession = AVAudioSession.sharedInstance()
-         
-         do {
-             // Configuramos la sesión de audio para grabación, modo de medición y con la opción de reducir el volumen de otros sonidos
-             try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
-             // Activamos la sesión de audio
-             try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
-             
-         } catch {
-             // Si ocurre un error al activar la sesión de audio, lo publicamos a través del speechErrorPublisher
-             speechErrorPublisher.send(())
-             return
-         }
-         
-         // Creamos la solicitud de reconocimiento de audio en búfer
-         recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
-         
-         guard let recognitionRequest = recognitionRequest else {
-             // Si no se puede crear la solicitud, terminamos el proceso con un fatalError
-             fatalError("No se pudo crear la solicitud de reconocimiento.")
-         }
-         
-         // Obtenemos el nodo de entrada de audio del motor de audio
-         let inputNode = audioEngine.inputNode
-         // Configuramos la solicitud para que informe los resultados parciales
-         recognitionRequest.shouldReportPartialResults = true
-         // Creamos la tarea de reconocimiento, pasando la solicitud y un manejador de resultados
-         recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { result, error in
-             // Si obtenemos un resultado, publicamos la transcripción parcial o completa a través del speechResultPublisher
-             if let result = result {
-                 DispatchQueue.main.async {
-                     // Publicamos la mejor transcripción obtenida (formattedString) al subscriber del publisher speechResultPublisher
-                     // Esto asegura que cualquier suscriptor reciba el texto reconocido
-                     self.speechResultPublisher.send(result.bestTranscription.formattedString)
-                 }
-             }
-             // Si hay un error o el resultado es final, detenemos el motor de audio y limpiamos las solicitudes y tareas
-             if error != nil || result?.isFinal == true {
-                 // Detenemos el motor de audio, lo cual también detiene la captación del micrófono
-                 self.audioEngine.stop()
-                 // Eliminamos el tap instalado en el inputNode del motor de audio para dejar de recibir audio
-                 inputNode.removeTap(onBus: 0)
-                 // Limpiamos la solicitud de reconocimiento, ya que no es necesaria después de detenerse
-                 self.recognitionRequest = nil
-                 // Cancelamos la tarea de reconocimiento y la dejamos en nil para evitar posibles fugas de memoria
-                 self.recognitionTask = nil
-             }
-         })
-         
-         // Configuramos el formato de grabación de audio para el nodo de entrada
-         let recordingFormat = inputNode.outputFormat(forBus: 0)
-         // Instalamos un tap (escucha) en el bus 0 para capturar el audio
-         inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
-             // Agregamos los datos del buffer de audio a la solicitud de reconocimiento
-             self.recognitionRequest?.append(buffer)
-         }
-         
-         // Preparamos el motor de audio para la grabación
-         audioEngine.prepare()
-         
-         do {
-             // Iniciamos el motor de audio para empezar a capturar el audio
-             try audioEngine.start()
-         } catch {
-             // Si ocurre un error al iniciar el motor de audio, lo publicamos a través del speechErrorPublisher
-             self.speechErrorPublisher.send(())
-         }
-     }
-     
-     private func stoptRecognition() {
-         // Detenemos el motor de audio
-         audioEngine.stop()
-         // Finalizamos el flujo de audio de la solicitud de reconocimiento
-         recognitionRequest?.endAudio()
-         // Cancelamos la tarea de reconocimiento activa
-         recognitionTask?.cancel()
-         recognitionTask = nil
-         // Desactivamos la sesión de audio
-         let audioSession = AVAudioSession.sharedInstance()
-         do {
-             try audioSession.setActive(false, options: .notifyOthersOnDeactivation)
-         } catch {
-             // Si ocurre un error al desactivar la sesión de audio, mostramos un mensaje de error
-             
-         }
-     }
-     
-     func requestAuthorization() {
-         startAuthorizationSubject.send(())
-     }
-     func requestStartRecognition() {
-         startRecognitionSubject.send(())
-     }
-     func requestStopRecognition() {
-         stoptRecognitionSubject.send(())
-     }
- }
- */
 class SpeechRecognizer {
     
     private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "es-ES"))
@@ -170,8 +17,7 @@ class SpeechRecognizer {
     private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
     private var recognitionTask: SFSpeechRecognitionTask?
     
-    private let recognitionTextSubject = PassthroughSubject<String, Never>()
-    private let errorSubject = PassthroughSubject<String?, Never>()
+    
     
     var recognitionTextPublisher: some Publisher<String, Never> {
         recognitionTextSubject.eraseToAnyPublisher()
@@ -179,8 +25,13 @@ class SpeechRecognizer {
     var errorPublisher: some Publisher<String?, Never> {
         errorSubject.eraseToAnyPublisher()
     }
+    var authorizationState: some Publisher<SFSpeechRecognizerAuthorizationStatus, Never> {
+        authorizationSpeechPublisher.eraseToAnyPublisher()
+    }
     
-    var authorizationSpeechPublisher: AnyPublisher<SFSpeechRecognizerAuthorizationStatus, Never> {
+    private let recognitionTextSubject = PassthroughSubject<String, Never>()
+    private let errorSubject = PassthroughSubject<String?, Never>()
+    private var authorizationSpeechPublisher: AnyPublisher<SFSpeechRecognizerAuthorizationStatus, Never> {
         return Deferred {
             Future { [weak self] promise in
                 SFSpeechRecognizer.requestAuthorization { status in
@@ -244,12 +95,12 @@ class SpeechRecognizer {
                     
                 }
             }
-                
+            
             
         }
         .eraseToAnyPublisher()  // Convertimos a AnyPublisher
     }
-  
+    
     func startRecognition() {
         
         guard audioEngine.isRunning == false else {return}
--- Voxi/SpeechViewModel.swift
+++ Voxi/SpeechViewModel.swift
@@ -10,109 +10,36 @@ import Combine
 import SwiftUI
 import Speech
 
-/*
- class SpeechViewModel: ObservableObject {
-     @Published var currentTranscription = ""
-     @Published var isAnalyzing = false
-     @Published var isAuthorized = false
-     
-     private let speechRecognizer = SpeechRecognizer()
-     private let startAnalysisSubject = PassthroughSubject<Void, Never>()
-     private let stopAnalysisSubject = PassthroughSubject<Void, Never>()
-     private let startAuthorizationSubject = PassthroughSubject<Void, Never>()
-     private var cancellables = Set<AnyCancellable>()
-     
-     init() {
-         //forma rapida de vincular authorizationStatus con isAuthorized y poder reaccionar a sus cambios
-         //speechRecognizer.authorizationStatusPublisher.assign(to: &$isAuthorized)
-         
-         //forma amplia de suscribir authorizationStatus con isAuthorized y poder reaccionar a sus cambios, ademas podemos agregar codigo lo que lo hace mas flexible
-         speechRecognizer.authorizationStatusPublisher.sink { [weak self] authorized in
-             DispatchQueue.main.async {
-                 DispatchQueue.main.async {
-                     self?.isAuthorized = authorized
-                 }
-                 
-             }
-         }
-         .store(in: &cancellables)
-         
-         speechRecognizer.speechResultPublisher.sink { [weak self] transcription in
-             DispatchQueue.main.async {
-                 self?.currentTranscription = transcription
-             }
-             
-         }
-         .store(in: &cancellables)
-         
-         speechRecognizer.speechErrorPublisher.sink {
-             print("Error")
-         }
-         .store(in: &cancellables)
-         
-         startAuthorizationSubject.sink { [weak self] in
-             self?.startAuthorization()
-         }
-         .store(in: &cancellables)
-         
-         startAnalysisSubject.sink { [weak self] in
-             self?.startAnalysis()
-         }
-         .store(in: &cancellables)
-         
-         stopAnalysisSubject.sink { [weak self] in
-             self?.stopAnalysis()
-         }
-         .store(in: &cancellables)
-         
-     }
-     
-     private func startAuthorization() {
-         speechRecognizer.requestAuthorization()
-     }
-     private func startAnalysis() {
-         
-         guard isAuthorized else { return }
-         speechRecognizer.requestStartRecognition()
-         DispatchQueue.main.async {
-             self.isAnalyzing = true
-         }
-     }
-     private func stopAnalysis() {
-         DispatchQueue.main.async {
-             self.isAnalyzing = false
-         }
-         speechRecognizer.requestStopRecognition()
-     }
-     
-     func requestStartAuthorization() {
-         startAuthorizationSubject.send(())
-     }
-     func requestStartAnalysis() {
-         startAnalysisSubject.send(())
-     }
-     func requestStopAnalysis() {
-         stopAnalysisSubject.send(())
-     }
-     func saveTranscription() {
-         
-     }
- }
-
- */
-
 final class SpeechViewModel: ObservableObject  {
+    //publishers
     @Published var isSpeechAuthorized: SFSpeechRecognizerAuthorizationStatus = .notDetermined
     @Published var isMicrophoneAuthorized: Bool = false
-    @Published var recognizedText: String = ""
-    @Published var buttonValue: Bool = false
     @Published var errorMessage: String? = nil
     @Published var hasError: Bool = false
-   
-    private var isAnalizing: Bool = false
     
-    private var speechRecognizer = SpeechRecognizer()
+    //UI
+    @Published var recognizedText: String = ""
+    @Published var buttonText: String = "Iniciar análisis"
+    @Published var buttonColor: Color = .green
+    @Published var buttonImageName: String = "mic.fill"
+    @Published var navigationLinkOpacity: Double = 1
+    @Published var buttonValue: Bool = false
+    
+    
+private var speechRecognizer = SpeechRecognizer()
     private var cancellables = Set<AnyCancellable>()
+    
+    private var stateButton: Bool = false {
+        didSet {
+            switch stateButton {
+                
+            case true:
+                updateRunningState()
+            case false:
+                updateToNotRunningState()
+            }
+        }
+    }
     
     init() {
         
@@ -141,29 +68,18 @@ final class SpeechViewModel: ObservableObject  {
             }
             .store(in: &cancellables)
         
-        $buttonValue
-            .receive(on: DispatchQueue.main)
-            .sink { [weak self] value in
-                print("Event: \(value)")
-                print("Llamado al boton")
-                if value {
-                    self?.startRecognition()
-                    print("Event advance if: \(value)")
-                } else {
-                    if self?.isSpeechAuthorized == .authorized && self?.isMicrophoneAuthorized == true {
-                        self?.stopRecognition()
-                        print("Event advance else if: \(value)")
-                        print("stop")
-                    }
-                }
-            }
-            .store(in: &cancellables)
         
         
     }
     
-   
-    func startRecognition() {
+    func saveTranscription() {
+        
+    }
+    func buttonTapped() {
+        stateButton = stateButton ? false : true
+    }
+    
+    private func updateRunningState() {
         
         switch isSpeechAuthorized {
             
@@ -171,7 +87,7 @@ final class SpeechViewModel: ObservableObject  {
             print("1")
             if !isMicrophoneAuthorized {
                 
-                speechRecognizer.authorizationSpeechPublisher
+                speechRecognizer.authorizationState
                     .receive(on: DispatchQueue.main)
                     .flatMap { [weak self] isSpeechAuthorized -> AnyPublisher<SFSpeechRecognizerAuthorizationStatus, Never> in
                         guard let self = self else {
@@ -214,6 +130,10 @@ final class SpeechViewModel: ObservableObject  {
                         if self.isSpeechAuthorized == .authorized && self.isMicrophoneAuthorized {
                             
                             speechRecognizer.startRecognition()
+                            buttonText = "Detener analisis"
+                            buttonColor = .red
+                            buttonImageName = "stop.fill"
+                            navigationLinkOpacity = 0
                             
                         } else {
                             self.buttonValue = false
@@ -235,6 +155,10 @@ final class SpeechViewModel: ObservableObject  {
             if isMicrophoneAuthorized {
                 print("4")
                 speechRecognizer.startRecognition()
+                buttonText = "Detener analisis"
+                buttonColor = .red
+                buttonImageName = "stop.fill"
+                navigationLinkOpacity = 0
             } else {
                 showMicrophonePermissionAlert()
             }
@@ -242,74 +166,18 @@ final class SpeechViewModel: ObservableObject  {
         @unknown default:
             print("4")
         }
-        
-        /*
-         if isSpeechAuthorized == .authorized && isMicrophoneAuthorized {
-                 print("1")
-                 speechRecognizer.startRecognition()
-                 
-         } else if isSpeechAuthorized == .authorized && !isMicrophoneAuthorized {
-                 print("2")
-                 
-                 
-         } else if isSpeechAuthorized == .notDetermined && isMicrophoneAuthorized {
-                 print("3")
-                 
-                 
-             } else if isSpeechAuthorized && !isMicrophoneAuthorized {
-                 print("4")
-                 speechRecognizer.authorizationSpeechPublisher
-                     .receive(on: DispatchQueue.main)
-                     .flatMap { [weak self] isSpeechAuthorized -> AnyPublisher<SFSpeechRecognizerAuthorizationStatus, Never> in
-                         guard let self = self else {
-                             return Just(SFSpeechRecognizerAuthorizationStatus.notDetermined).eraseToAnyPublisher()
-                         }
-                         
-                         
-                         print("Speech authorization: \(isSpeechAuthorized)")
-                         
-                         if isSpeechAuthorized == .authorized {
-                             self.isSpeechAuthorized = true
-                             // Usamos Deferred para que el Future del micrófono espere hasta que sea necesario
-                             return Deferred {
-                                 self.speechRecognizer.authorizationMicrophonePublisher
-                             }
-                             .eraseToAnyPublisher()
-                         } else {
-                             return Just(false).eraseToAnyPublisher()
-                         }
-                     }
-                     .receive(on: DispatchQueue.main)
-                     .sink { [weak self] isMicrophoneAuthorized in
-                         guard let self = self else {
-                             return
-                         }
-                         self.isMicrophoneAuthorized = isMicrophoneAuthorized
-                         print("Microphone authorization: \(isMicrophoneAuthorized)")
-                         
-                         if self.isSpeechAuthorized && self.isMicrophoneAuthorized {
-                             
-                             speechRecognizer.startRecognition()
-                             
-                         } else {
-                             self.buttonValue = false
-                             print("is anañizing \(buttonValue)")
-                         }
-                     }
-                     .store(in: &cancellables)
-             }
-
-         */
-        
+    }
+    private func updateToNotRunningState() {
+        if isSpeechAuthorized == .authorized && isMicrophoneAuthorized == true {
+            speechRecognizer.stopRecognition()
+            buttonText = "Iniciar análisis"
+            buttonColor = .green
+            buttonImageName = "mic.fill"
+            navigationLinkOpacity = 1
+        }
     }
     
-
-    func stopRecognition() {
-        speechRecognizer.stopRecognition()
-    }
-    func saveTranscription() {
-        
-    }
+    
     
     func showSpeechPermissionAlert() {
         // Mostrar una alerta para que el usuario vaya a Ajustes
